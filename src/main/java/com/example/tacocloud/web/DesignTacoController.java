fb5c3b42bbd3aac1c06b444eca462d34b6b485aa
Md-Nabil11 <mohammed.nabil@bkash.com>
2022-12-11 15:42:32 +0600

    Chapter 2

---
+++ src/main/java/com/example/tacocloud/web/DesignTacoController.java
@@ -0,0 +1,59 @@
+package com.example.tacocloud.web;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+
+import com.example.tacocloud.Ingredient;
+import com.example.tacocloud.Taco;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
+import com.example.tacocloud.Ingredient.Type;
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            List<Ingredient>in = ingredients.stream()
+                            .filter(i->i.getType().toString() == type.toString())
+                            .collect(Collectors.toList());
+
+            model.addAttribute(type.toString().toLowerCase(), in);
+        }
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+            //return "redirect:/design";
+        }
+        // Save the taco design...
+        // We'll do this in chapter 3
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
+}
