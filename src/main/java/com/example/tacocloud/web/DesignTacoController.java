b7d731d91affc5b3ca31e34c9e9502fa3630f994
Md-Nabil11 <mohammed.nabil@bkash.com>
2022-12-12 15:38:42 +0600

    Chapter 3

--- src/main/java/com/example/tacocloud/web/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/web/DesignTacoController.java
@@ -1,59 +1,82 @@
 package com.example.tacocloud.web;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import com.example.tacocloud.Ingredient;
+import com.example.tacocloud.Order;
 import com.example.tacocloud.Taco;
+import com.example.tacocloud.data.IngredientRepository;
+import com.example.tacocloud.data.TacoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 import com.example.tacocloud.Ingredient.Type;
+
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
-    @GetMapping
-    public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
-        Type[] types = Ingredient.Type.values();
-        for (Type type : types) {
-            List<Ingredient>in = ingredients.stream()
-                            .filter(i->i.getType().toString() == type.toString())
-                            .collect(Collectors.toList());
-
-            model.addAttribute(type.toString().toLowerCase(), in);
-        }
-        model.addAttribute("design", new Taco());
-        return "design";
+    private final IngredientRepository ingredientRepo;
+    private TacoRepository tacoRepo;
+    @Autowired
+    public DesignTacoController(
+            IngredientRepository ingredientRepo,
+            TacoRepository tacoRepo) {
+        this.ingredientRepo = ingredientRepo;
+        this.tacoRepo = tacoRepo;
     }
 
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "design")
+    public Taco design() {
+        return new Taco();
+    }
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+
+        return "design";
+    }
     @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors) {
+    public String processDesign(
+            @Valid Taco taco, Errors errors,
+            @ModelAttribute Order order) {
+
         if (errors.hasErrors()) {
             return "design";
-            //return "redirect:/design";
         }
-        // Save the taco design...
-        // We'll do this in chapter 3
-        log.info("Processing design: " + design);
+
+        Taco saved = tacoRepo.save(taco);
+        order.addDesign(saved);
+
         return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
     }
 }
